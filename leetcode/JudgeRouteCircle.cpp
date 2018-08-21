cd2f1ddeb40597b1b6758aca7550d4bdc5c01f99
NazymBerdaliyeva <nazym.berdalyeva@gmail.com>
2018-08-21 06:43:32 +0600

    Add files via upload
    Leetcode: 657. Judge Route Circle

---
+++ leetcode/JudgeRouteCircle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        bool makesCircle =false;
+        int x=0,y=0;
+        for(int i=0; i<moves.size(); i++) {
+            if(moves[i] == 'U') y++;
+            else if(moves[i] == 'D') y--;
+            else if(moves[i] == 'R') x++;
+            else if(moves[i] == 'L') x--;
+        }
+           
+        return x == 0 && y == 0;
+    }
+};
