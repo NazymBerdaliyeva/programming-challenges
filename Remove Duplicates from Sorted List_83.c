73801e04f843847600290123b352d491f8d6e391
NazymBerdaliyeva <nazym.berdalyeva@gmail.com>
2019-07-26 17:29:26 +0600

    Leetcode: 83. Remove Duplicates from Sorted List

---
+++ Remove Duplicates from Sorted List_83.c
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        
+        while(temp!=NULL){
+           while(temp->next!=NULL && temp->val == temp->next->val){
+               temp->next = temp->next->next;
+             }
+           temp = temp->next;
+        }
+        return head;
+    }
+};
