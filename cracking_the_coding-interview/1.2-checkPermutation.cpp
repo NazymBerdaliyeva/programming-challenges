4f88a4ba3f20425ada8805314daf0e87e42551ce
Nazym <nazym.berdalyeva@gmail.com>
2020-04-18 18:45:27 +0600

    18.04.2020

--- cracking_the_coding-interview/1.2-checkPermutation.cpp
+++ cracking_the_coding-interview/1.2-checkPermutation.cpp
@@ -20,8 +20,8 @@ bool isPermutation1(string s1, string s2) {
 }
 bool isPermutation2(string s1, string s2) {
     if(s1.length() != s2.length())
-        return false;
-        
+           return false;
+    
     int letters[127];
     for(int i=0; i<s1.length(); i++) {
         int c = s1[i] - 48;
@@ -29,6 +29,7 @@ bool isPermutation2(string s1, string s2) {
     }
     for(int i=0; i<s2.length();i++) {
         int c = s2[i] - 48;
+        letters[c]--;
         if(letters[c] < 0)
             return false;
     }
@@ -36,7 +37,7 @@ bool isPermutation2(string s1, string s2) {
 }
 int main(int argc, const char * argv[]) {
     string s1 = "qwertyuiop";
-    string s2 = "poiuytrewq";
+    string s2 = "qwertyuiop";
     
     if(isPermutation2(s1,s2))
         cout<<"Yes";
